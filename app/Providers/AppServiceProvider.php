1402b944172b783cb705e8f76fe548fd153d0cf4
Eng.HamzaAshrafi <hamza.3ashrafy@gmail.com>
2026-07-16 10:12:48 +0300

    feat: install Laravel Sanctum, add PersonalAccessToken model with UUID, register in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,12 @@
 
 namespace App\Providers;
 
+use App\Models\PersonalAccessToken;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Sanctum\Sanctum;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +24,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Sanctum::usePersonalAccessTokenModel(PersonalAccessToken::class);
+
+        Model::preventLazyLoading(! $this->app->isProduction());
+        Model::preventSilentlyDiscardingAttributes(! $this->app->isProduction());
+
+        Vite::useAggressivePrefetching();
+
+        if ($this->app->isProduction()) {
+            URL::forceScheme('https');
+        }
     }
 }
